15247ec3ee3181a8b050d095c7453e90c16bc65c
Igor Wiedler <igor@wiedler.ch>
2013-12-31 01:13:14 +0100

    add busy beaver example

---
+++ examples/busy-beaver.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use igorw\turing as t;
+
+// 3-state busy beaver
+// from wikipedia
+//   http://en.wikipedia.org/wiki/Turing_machine_examples
+
+$accept_states = ['D'];
+
+// quintuple
+// state, read condition, write value, move direction, new state
+$rules = [
+    ['A', '_', '1', 'r', 'B'],
+    ['A', '1', '1', 'l', 'C'],
+    ['B', '_', '1', 'l', 'A'],
+    ['B', '1', '1', 'r', 'B'],
+    ['C', '_', '1', 'l', 'B'],
+    ['C', '1', '1', 'n', 'D'],
+];
+
+$tape = [];
+
+$position = 3;
+$state = 'A';
+
+t\run_debug(
+    $rules,
+    $accept_states,
+    new t\Config($tape, $position, $state)
+);
